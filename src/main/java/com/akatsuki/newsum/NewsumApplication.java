39b1be339db1536db16f71f2bf30ee247c52ddbc
Han Ju Kim <hjkim2902@gmail.com>
2025-04-30 19:06:15 +0900

    feat : Application 이름 수정 및 Timezone Asia/Seoul 설정

--- src/main/java/com/akatsuki/newsum/NewsumApplication.java
+++ src/main/java/com/akatsuki/newsum/NewsumApplication.java
@@ -3,11 +3,14 @@ package com.akatsuki.newsum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
-public class NewsumBeApplication {
+public class NewsumApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NewsumBeApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        SpringApplication.run(NewsumApplication.class, args);
     }
 
 }
