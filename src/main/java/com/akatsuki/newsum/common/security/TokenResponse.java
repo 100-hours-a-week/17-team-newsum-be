102165d74ead303352650a86695a9fef5b00f936
gongsua <91416981+gongsua@users.noreply.github.com>
2025-05-02 15:02:06 +0900

    remove SuccessHandler method

---
+++ src/main/java/com/akatsuki/newsum/common/security/TokenResponse.java
@@ -0,0 +1,15 @@
+package com.akatsuki.newsum.common.security;
+
+import lombok.Getter;
+
+@Getter
+public class TokenResponse {
+
+    private String accessToken;
+    private String refreshToken;
+
+    public TokenResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}
--- src/main/java/com/akatsuki/newsum/config/SecurityConfig.java
+++ src/main/java/com/akatsuki/newsum/config/SecurityConfig.java
@@ -16,20 +16,24 @@ public class SecurityConfig {
 	private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
 	//TODO: 설정 필요
-	private final String[] excludePaths = {"/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/error",
+	private final String[] excludePaths = {
+		"/swagger-ui/**",
+		"/v3/api-docs/**",
+		"/swagger-resources/**",
+		"/error",
+		"/api/oauth/login/google", // ✅ 프론트에서 인가코드 받아서 보내는 API
+		"/api/oauth/login/kakao",  // (추후 카카오도 같이 처리)
 		"/**"};
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
+			.csrf(csrf -> csrf.disable())
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(excludePaths)
 				.permitAll()
 				.anyRequest()
 				.authenticated()
-			)
-			.oauth2Login(oauth2 -> oauth2
-				.successHandler(oAuth2LoginSuccessHandler) // 로그인 성공 시 핸들러
 			);
 
 		return http.build();
