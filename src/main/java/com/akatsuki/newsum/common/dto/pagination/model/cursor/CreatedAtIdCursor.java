ce3159aafc13197e76dafc29c9484604610bae8c
Han Ju Kim <hjkim2902@gmail.com>
2025-05-03 23:06:56 +0900

    feat : CreatedAtIdCursor 추가

---
+++ src/main/java/com/akatsuki/newsum/common/dto/pagination/model/cursor/CreatedAtIdCursor.java
@@ -0,0 +1,24 @@
+package com.akatsuki.newsum.common.dto.pagination.model.cursor;
+
+import java.time.LocalDateTime;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CreatedAtIdCursor implements Cursor {
+	private LocalDateTime createdAt;
+	private Long id;
+
+	public CreatedAtIdCursor(LocalDateTime createdAt, Long id) {
+		this.createdAt = createdAt;
+		this.id = id;
+	}
+
+	@Override
+	public String getCursor() {
+		return createdAt.toString() + "_" + id;
+	}
+}
