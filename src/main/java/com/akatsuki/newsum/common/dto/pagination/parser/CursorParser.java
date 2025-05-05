8a603d29f8dcd910c993016cf61d31f2b8aeb6e8
Han Ju Kim <hjkim2902@gmail.com>
2025-05-05 17:56:02 +0900

    refactor : cursorParser -> CursorDeserializer 수정

--- src/main/java/com/akatsuki/newsum/common/dto/pagination/parser/CursorParser.java
+++
@@ -1,9 +0,0 @@
-package com.akatsuki.newsum.common.dto.pagination.parser;
-
-import com.akatsuki.newsum.common.dto.pagination.model.cursor.Cursor;
-
-public interface CursorParser {
-	boolean supports(String cursor);
-
-	Cursor parse(String cursor);
-}
---
+++ src/main/java/com/akatsuki/newsum/common/pagination/deserializer/CreatedAtIdCursorDeserializer.java
@@ -0,0 +1,34 @@
+package com.akatsuki.newsum.common.pagination.deserializer;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Component;
+
+import com.akatsuki.newsum.common.pagination.model.cursor.CreatedAtIdCursor;
+import com.akatsuki.newsum.common.pagination.model.cursor.Cursor;
+
+@Component
+public class CreatedAtIdCursorDeserializer implements CursorDeserializer<CreatedAtIdCursor> {
+
+	private final CreatedAtIdCursor defaultCursor = new CreatedAtIdCursor(LocalDateTime.of(2000, 1, 1, 0, 0, 0), 0L);
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+
+	@Override
+	public boolean supports(Class<? extends Cursor> cursorType) {
+		return cursorType.equals(CreatedAtIdCursor.class);
+	}
+
+	@Override
+	public CreatedAtIdCursor deserialize(String rawCursor) {
+		String[] parts = rawCursor.trim().split("_");
+		LocalDateTime createdAt = LocalDateTime.parse(parts[0], formatter);
+		Long id = Long.parseLong(parts[1]);
+		return new CreatedAtIdCursor(createdAt, id);
+	}
+
+	@Override
+	public CreatedAtIdCursor defaultCursor() {
+		return defaultCursor;
+	}
+}
