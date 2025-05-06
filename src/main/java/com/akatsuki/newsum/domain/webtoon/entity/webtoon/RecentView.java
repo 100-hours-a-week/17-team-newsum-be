8f28220a6101da1254abf850b1ff398be28dd3a4
Han Ju Kim <hjkim2902@gmail.com>
2025-05-06 23:44:54 +0900

    feat : RecentView Entity 연관관계 추가

--- src/main/java/com/akatsuki/newsum/domain/webtoon/entity/webtoon/RecentView.java
+++ src/main/java/com/akatsuki/newsum/domain/webtoon/entity/webtoon/RecentView.java
@@ -2,14 +2,18 @@ package com.akatsuki.newsum.domain.webtoon.entity.webtoon;
 
 import java.time.LocalDateTime;
 
+import com.akatsuki.newsum.domain.user.entity.User;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,19 +27,14 @@ public class RecentView {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "user_id", nullable = false)
-	private Long userId;
+	@JoinColumn(name = "user_id", nullable = false)
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
 
-	@Column(name = "webtoon_id", nullable = false)
-	private Long webtoonId;
+	@JoinColumn(name = "webtoon_id", nullable = false)
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Webtoon webtoon;
 
 	@Column(name = "viewed_at", nullable = false)
 	private LocalDateTime viewedAt;
-
-	@Builder
-	public RecentView(Long userId, Long webtoonId) {
-		this.userId = userId;
-		this.webtoonId = webtoonId;
-		this.viewedAt = LocalDateTime.now();
-	}
 }
