94bf64caf3ac894635d8e16e9179f46165c5cbe7
Han Ju Kim <hjkim2902@gmail.com>
2025-05-06 19:00:08 +0900

    feat : 웹툰 목록 조회 API 추가

--- src/main/java/com/akatsuki/newsum/domain/webtoon/controller/WebtoonController.java
+++ src/main/java/com/akatsuki/newsum/domain/webtoon/controller/WebtoonController.java
@@ -1,17 +1,27 @@
 package com.akatsuki.newsum.domain.webtoon.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.akatsuki.newsum.common.dto.ApiResponse;
 import com.akatsuki.newsum.common.dto.ResponseCodeAndMessage;
+import com.akatsuki.newsum.common.pagination.CursorPaginationService;
+import com.akatsuki.newsum.common.pagination.annotation.CursorParam;
+import com.akatsuki.newsum.common.pagination.model.cursor.CreatedAtIdCursor;
+import com.akatsuki.newsum.common.pagination.model.cursor.Cursor;
+import com.akatsuki.newsum.common.pagination.model.page.CursorPage;
 import com.akatsuki.newsum.common.security.JwtTokenUtil;
 import com.akatsuki.newsum.common.security.TokenProvider;
+import com.akatsuki.newsum.domain.webtoon.dto.WebtoonCardDto;
 import com.akatsuki.newsum.domain.webtoon.dto.WebtoonDetailResponse;
+import com.akatsuki.newsum.domain.webtoon.dto.WebtoonListResponse;
 import com.akatsuki.newsum.domain.webtoon.dto.WebtoonResponse;
 import com.akatsuki.newsum.domain.webtoon.service.WebtoonService;
 
@@ -23,7 +33,24 @@ import lombok.RequiredArgsConstructor;
 public class WebtoonController {
 
 	private final WebtoonService webtoonService;
+	private final CursorPaginationService cursorPaginationService;
 	private final TokenProvider tokenProvider;
+
+	@GetMapping
+	public ResponseEntity<ApiResponse<WebtoonListResponse>> getWebtoons(
+		//TODO : 추후 키워드, AI작가로 조회 가능
+		@RequestParam(required = false) String category,
+		@CursorParam(cursorType = CreatedAtIdCursor.class) Cursor cursor,
+		@RequestParam(defaultValue = "10") int size
+	) {
+		List<WebtoonCardDto> result = webtoonService.findWebtoonsByCategory(category, cursor, size);
+		CursorPage<WebtoonCardDto> cursorPage = cursorPaginationService.create(result, size, cursor);
+		WebtoonListResponse response = WebtoonListResponse.of(cursorPage);
+
+		return ResponseEntity.ok(
+			ApiResponse.success(ResponseCodeAndMessage.WEBTOON_LIST_SUCCESS, response)
+		);
+	}
 
 	@GetMapping("/{webtoonId}")
 	public ResponseEntity<ApiResponse<WebtoonResponse>> getWebtoon(
