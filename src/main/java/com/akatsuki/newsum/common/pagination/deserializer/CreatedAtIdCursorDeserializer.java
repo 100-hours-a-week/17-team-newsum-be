4f46faa14dffcd3f165abee7d95719696599e6a6
Han Ju Kim <hjkim2902@gmail.com>
2025-05-06 18:58:49 +0900

    fix : CreatedAtIdCursorDeserializer 파싱 포맷 수정

--- src/main/java/com/akatsuki/newsum/common/pagination/deserializer/CreatedAtIdCursorDeserializer.java
+++ src/main/java/com/akatsuki/newsum/common/pagination/deserializer/CreatedAtIdCursorDeserializer.java
@@ -1,7 +1,6 @@
 package com.akatsuki.newsum.common.pagination.deserializer;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import org.springframework.stereotype.Component;
 
@@ -12,7 +11,6 @@ import com.akatsuki.newsum.common.pagination.model.cursor.Cursor;
 public class CreatedAtIdCursorDeserializer implements CursorDeserializer<CreatedAtIdCursor> {
 
 	private final CreatedAtIdCursor defaultCursor = new CreatedAtIdCursor(LocalDateTime.of(2000, 1, 1, 0, 0, 0), 0L);
-	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
 
 	@Override
 	public boolean supports(Class<? extends Cursor> cursorType) {
@@ -22,7 +20,7 @@ public class CreatedAtIdCursorDeserializer implements CursorDeserializer<Created
 	@Override
 	public CreatedAtIdCursor deserialize(String rawCursor) {
 		String[] parts = rawCursor.trim().split("_");
-		LocalDateTime createdAt = LocalDateTime.parse(parts[0], formatter);
+		LocalDateTime createdAt = LocalDateTime.parse(parts[0]);
 		Long id = Long.parseLong(parts[1]);
 		return new CreatedAtIdCursor(createdAt, id);
 	}
