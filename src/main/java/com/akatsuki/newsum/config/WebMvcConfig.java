e837d2e4cafeede5b3c7825cfad2b011075ecfaa
gongsua <91416981+gongsua@users.noreply.github.com>
2025-05-08 18:52:52 +0900

    fix : /oauth2/callback/kakao 통합 서빙 라우팅 예외

--- src/main/java/com/akatsuki/newsum/config/WebMvcConfig.java
+++ src/main/java/com/akatsuki/newsum/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.akatsuki.newsum.common.pagination.resolver.CursorArgumentResolver;
@@ -19,5 +21,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(cursorArgumentResolver);
+	}
+
+	// 정적 리소스 처리: /static, /favicon.ico 등
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+			.addResourceLocations("classpath:/static/");
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/oauth2/callback/**")
+			.setViewName("forward:/index.html");
 	}
 }
