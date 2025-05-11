bd2bc737564b24cb221dc2f5a1d44b7e73e78fb5
gongsua <91416981+gongsua@users.noreply.github.com>
2025-05-11 21:48:43 +0900

    유저프로필사진 유효성 url 삭제

--- src/main/java/com/akatsuki/newsum/domain/user/dto/UpdateUserRequestDto.java
+++ src/main/java/com/akatsuki/newsum/domain/user/dto/UpdateUserRequestDto.java
@@ -1,14 +1,10 @@
 package com.akatsuki.newsum.domain.user.dto;
 
-import org.hibernate.validator.constraints.URL;
-
 import jakarta.validation.constraints.Size;
 
 public record UpdateUserRequestDto(
 	@Size(min = 1, max = 16)
 	String nickname,
-
-	@URL
 	String profileImage
 ) {
 }
