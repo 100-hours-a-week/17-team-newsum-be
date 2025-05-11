19dab9486872b329267ca955a09f57063e5ff3cf
gongsua <91416981+gongsua@users.noreply.github.com>
2025-05-11 21:21:12 +0900

    updateuserREquestDTO에 사용자 유효성 검사 추가

--- src/main/java/com/akatsuki/newsum/domain/user/dto/UpdateUserRequestDto.java
+++ src/main/java/com/akatsuki/newsum/domain/user/dto/UpdateUserRequestDto.java
@@ -1,7 +1,14 @@
 package com.akatsuki.newsum.domain.user.dto;
 
+import org.hibernate.validator.constraints.URL;
+
+import jakarta.validation.constraints.Size;
+
 public record UpdateUserRequestDto(
+	@Size(min = 1, max = 16)
 	String nickname,
+
+	@URL
 	String profileImage
 ) {
 }
