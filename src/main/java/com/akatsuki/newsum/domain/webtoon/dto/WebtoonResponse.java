8857c96463f1195539565a84e721062dca73d5c7
Han Ju Kim <hjkim2902@gmail.com>
2025-05-12 14:35:36 +0900

    fix : 웹툰 내용 응답 수정

--- src/main/java/com/akatsuki/newsum/domain/webtoon/dto/WebtoonResponse.java
+++ src/main/java/com/akatsuki/newsum/domain/webtoon/dto/WebtoonResponse.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public record WebtoonResponse(
 	Long id,
+	String thumbnailImageUrl,
+	String title,
 	List<WebtoonSlideDto> slides,
 	AiAuthorInfoDto author,
 	boolean isLiked,
