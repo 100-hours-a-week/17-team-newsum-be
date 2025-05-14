bda992e689385ec3e530c49d2d18a86e669b6402
gongsua <91416981+gongsua@users.noreply.github.com>
2025-05-14 13:27:20 +0900

    feat: webtoon 좋아요 레퍼지토리 생성

---
+++ src/main/java/com/akatsuki/newsum/domain/webtoon/repository/WebtoonLikeRepository.java
@@ -0,0 +1,13 @@
+package com.akatsuki.newsum.domain.webtoon.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.akatsuki.newsum.domain.user.entity.User;
+import com.akatsuki.newsum.domain.webtoon.entity.webtoon.Webtoon;
+import com.akatsuki.newsum.domain.webtoon.entity.webtoon.WebtoonLike;
+
+public interface WebtoonLikeRepository extends JpaRepository<WebtoonLike, Long> {
+	Optional<WebtoonLike> findByWebtoonandUser(Webtoon webtoon, User user);
+}
