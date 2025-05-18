03243096402f90102a32b888b7b1fac9476ea289
gongsua <91416981+gongsua@users.noreply.github.com>
2025-05-18 23:07:15 +0900

    feat : 로그 레벨 수정

--- src/test/java/com/akatsuki/newsum/NewsumApplicationTests.java
+++ src/test/java/com/akatsuki/newsum/NewsumApplicationTests.java
@@ -1,7 +1,9 @@
 package com.akatsuki.newsum;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 class NewsumApplicationTests {
 
 	@Test
