7e83aee188151a6e3bea7433b7fda7c61e89763c
gongsua <91416981+gongsua@users.noreply.github.com>
2025-05-20 11:47:39 +0900

    feat : 웹툰 북마크 repository 구현

---
+++ src/main/java/com/akatsuki/newsum/domain/webtoon/repository/WebtoonFavoriteRepository.java
@@ -0,0 +1,19 @@
+package com.akatsuki.newsum.domain.webtoon.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.akatsuki.newsum.domain.webtoon.entity.webtoon.WebtoonFavorite;
+
+public interface WebtoonFavoriteRepository extends JpaRepository<WebtoonFavorite, Long> {
+
+	//상세페이지 들어갔을때 북마크했는지 아닌지 확인및 북마크 할 용도
+	boolean existsByWebtoonIdAndUserId(Long webtoonId, Long userId);
+
+	//유저가 북마크한 전체 웹툰들 북마크 페이지에서 출력
+	List<WebtoonFavorite> findByUserId(Long userId);
+
+	void deleteByUserIdAndWebtoonId(Long userId, Long webtoonId);
+
+}
