a411d9d8a4289db07a02d1a59b85cfeba2556fdb
gongsua <91416981+gongsua@users.noreply.github.com>
2025-05-27 19:18:58 +0900

    chore: flyway 파일 수정

--- src/main/resources/db/migration/V2025.05.27.1__create_webtoon_like_table.sql
+++ src/main/resources/db/migration/V2025.05.27.1__create_webtoon_like_table.sql
@@ -7,3 +7,6 @@ CREATE TABLE webtoon_like
     CONSTRAINT fk_webtoon_like_user FOREIGN KEY (user_id) REFERENCES users (id),
     CONSTRAINT fk_webtoon_like_webtoon FOREIGN KEY (webtoon_id) REFERENCES webtoon (id)
 );
+
+ALTER TABLE webtoon_like
+    ADD CONSTRAINT uq_user_webtoon_like UNIQUE (user_id, webtoon_id);
