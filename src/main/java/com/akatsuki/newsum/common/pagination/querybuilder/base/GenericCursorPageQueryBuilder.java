d4b5342b6151c74daddf4c0e0da203c9e2a435c6
Han Ju Kim <hjkim2902@gmail.com>
2025-05-29 21:04:31 +0900

    feat : GenericCursorPageQueryBuilder 추상 클래스 추가

---
+++ src/main/java/com/akatsuki/newsum/common/pagination/querybuilder/base/GenericCursorPageQueryBuilder.java
@@ -0,0 +1,33 @@
+package com.akatsuki.newsum.common.pagination.querybuilder.base;
+
+import com.akatsuki.newsum.common.pagination.model.cursor.Cursor;
+import com.akatsuki.newsum.common.pagination.model.query.QueryEngineType;
+import com.akatsuki.newsum.common.pagination.model.query.QueryFragment;
+import com.akatsuki.newsum.common.pagination.querybuilder.CursorPageQueryBuilder;
+
+public abstract class GenericCursorPageQueryBuilder<T extends Cursor> implements CursorPageQueryBuilder<T> {
+	@Override
+	public boolean supports(Class<?> domainClass, Class<? extends Cursor> cursorType, QueryEngineType engineType) {
+		return engineType.equals(getEngineType())
+			&& cursorType.equals(getCursorType())
+			&& domainClass.equals(getDomainClass());
+	}
+
+	@Override
+	public QueryFragment buildQueryFragment(T cursor) {
+		return new QueryFragment(
+			buildWhereClause(cursor),
+			buildOrderByClause(cursor)
+		);
+	}
+
+	protected abstract QueryEngineType getEngineType();
+
+	protected abstract Class<? extends Cursor> getCursorType();
+
+	protected abstract Class<?> getDomainClass();
+
+	protected abstract Object buildWhereClause(T cursor);
+
+	protected abstract Object buildOrderByClause(T cursor);
+}
