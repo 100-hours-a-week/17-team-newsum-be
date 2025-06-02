025d157df04d6631b0d06bcc83595bb0fdcbb1a2
gongsua <91416981+gongsua@users.noreply.github.com>
2025-06-02 15:07:20 +0900

    feat : keyword 관련 flyway 파일 생성

---
+++ src/main/resources/db/migration/V2025.06.02.1__Create_keyword_and_keyword_favorite.sql
@@ -0,0 +1,21 @@
+CREATE TABLE keyword
+(
+    id      BIGSERIAL PRIMARY KEY,
+    content VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE keyword_favorite
+(
+    id         BIGSERIAL PRIMARY KEY,
+    user_id    BIGINT    NOT NULL,
+    keyword_id BIGINT    NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_keyword_favorite_user
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+
+    CONSTRAINT fk_keyword_favorite_keyword
+        FOREIGN KEY (keyword_id) REFERENCES keyword (id) ON DELETE CASCADE,
+
+    CONSTRAINT unique_user_keyword UNIQUE (user_id, keyword_id)
+);
