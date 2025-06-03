d6bfb3dbe5603a90af6da700be5c22bfb9943fd7
gongsua <91416981+gongsua@users.noreply.github.com>
2025-06-03 19:47:21 +0900

    refactor : 데이터 무결성 예외 사용하여 로직 개편

--- src/main/java/com/akatsuki/newsum/domain/webtoon/repository/KeywordFavoriteRepository.java
+++ src/main/java/com/akatsuki/newsum/domain/webtoon/repository/KeywordFavoriteRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.akatsuki.newsum.domain.webtoon.entity.webtoon.KeywordFavorite;
 
 public interface KeywordFavoriteRepository extends JpaRepository<KeywordFavorite, Long> {
+	boolean existsByUserIdAndKeywordId(Long userId, Long keywordId);
 }
