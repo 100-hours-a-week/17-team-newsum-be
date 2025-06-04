6abbe612c3f2b8444652b1d40bb1239b14f09993
gongsua <91416981+gongsua@users.noreply.github.com>
2025-06-04 13:43:12 +0900

    feat : 삭제 성공 메세지 추가

--- src/main/java/com/akatsuki/newsum/common/dto/ResponseCodeAndMessage.java
+++ src/main/java/com/akatsuki/newsum/common/dto/ResponseCodeAndMessage.java
@@ -47,7 +47,8 @@ public enum ResponseCodeAndMessage {
 	NOTI_LIST_SUCCESS(HttpStatus.OK.value(), "알림 목록 조회에 성공했습니다."),
 
 	//키워드 성공 응답
-	KEYWORD_SUBSCRIBE_SUCCESS(HttpStatus.OK.value(), "키워드 구독에 성공했습니다.");
+	KEYWORD_SUBSCRIBE_SUCCESS(HttpStatus.OK.value(), "키워드 구독에 성공했습니다."),
+	KEYWORD_SUBSCRIBE_DELETE_SUCCESS(HttpStatus.OK.value(), "키워드 구독에 삭제에 성공했습니다.");
 
 	private final int code;
 	private final String message;
