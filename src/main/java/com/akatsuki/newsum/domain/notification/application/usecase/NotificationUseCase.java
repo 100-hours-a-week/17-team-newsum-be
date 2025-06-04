b41420e9d7e1591fe4dd55daf6f5ba50cd538a19
Han Ju Kim <hjkim2902@gmail.com>
2025-06-04 09:43:29 +0900

    feat: NotificationUseCase 및 서비스 추가

---
+++ src/main/java/com/akatsuki/newsum/domain/notification/application/usecase/NotificationUseCase.java
@@ -0,0 +1,17 @@
+package com.akatsuki.newsum.domain.notification.application.usecase;
+
+import com.akatsuki.newsum.domain.notification.application.dto.NotificationPageListCommand;
+import com.akatsuki.newsum.domain.notification.application.dto.NotificationPageListResult;
+import com.akatsuki.newsum.domain.notification.application.dto.ReplyNotificationCommand;
+
+public interface NotificationUseCase {
+	NotificationPageListResult lookupNotificationsByUserId(NotificationPageListCommand notificationPageListCommand);
+
+	void notify(ReplyNotificationCommand replyNotificationCommand);
+
+	void removeAllNotifications(Long userId);
+
+	void removeNotification(Long userId, Long notificationId);
+
+	void readNotification(Long userId, Long notificationId);
+}
