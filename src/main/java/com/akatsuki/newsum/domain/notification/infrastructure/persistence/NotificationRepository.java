38f434e4b293f3c36d4349ec70331f7e3fe08809
Han Ju Kim <hjkim2902@gmail.com>
2025-06-04 09:46:07 +0900

    feat: 알림 DAO 로직 수정

--- src/main/java/com/akatsuki/newsum/domain/notification/infrastructure/persistence/NotificationRepository.java
+++ src/main/java/com/akatsuki/newsum/domain/notification/infrastructure/persistence/NotificationRepository.java
@@ -1,11 +1,21 @@
 package com.akatsuki.newsum.domain.notification.infrastructure.persistence;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.akatsuki.newsum.common.pagination.model.cursor.Cursor;
+import com.akatsuki.newsum.domain.notification.domain.Notification;
 import com.akatsuki.newsum.domain.notification.infrastructure.persistence.dto.NotificationReadModel;
 import com.akatsuki.newsum.domain.user.entity.User;
 
 public interface NotificationRepository {
 	List<NotificationReadModel> findByUserWithCursorAndSize(User user, Cursor cursor, int size);
+
+	Notification save(Notification notification);
+
+	void deleteAllByUser(User user);
+
+	void delete(Notification notification);
+
+	Optional<Notification> findNotificationByIdAndUserById(Long id, Long userId);
 }
--- src/main/java/com/akatsuki/newsum/domain/notification/infrastructure/persistence/NotificationRepositoryImpl.java
+++ src/main/java/com/akatsuki/newsum/domain/notification/infrastructure/persistence/NotificationRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.akatsuki.newsum.domain.notification.infrastructure.persistence;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
 import com.akatsuki.newsum.common.pagination.model.cursor.Cursor;
+import com.akatsuki.newsum.domain.notification.domain.Notification;
 import com.akatsuki.newsum.domain.notification.infrastructure.persistence.dto.NotificationReadModel;
 import com.akatsuki.newsum.domain.notification.infrastructure.persistence.jpa.NotificationJpaRepository;
 import com.akatsuki.newsum.domain.user.entity.User;
@@ -20,5 +22,25 @@ public class NotificationRepositoryImpl implements NotificationRepository {
 	@Override
 	public List<NotificationReadModel> findByUserWithCursorAndSize(User user, Cursor cursor, int size) {
 		return notificationJpaRepository.findByUserWithCursorAndSize(user, cursor, size);
+	}
+
+	@Override
+	public Notification save(Notification notification) {
+		return notificationJpaRepository.save(notification);
+	}
+
+	@Override
+	public void deleteAllByUser(User user) {
+		notificationJpaRepository.deleteAllByUser(user);
+	}
+
+	@Override
+	public void delete(Notification notification) {
+		notificationJpaRepository.delete(notification);
+	}
+
+	@Override
+	public Optional<Notification> findNotificationByIdAndUserById(Long id, Long userId) {
+		return notificationJpaRepository.findByIdAndUserId(id, userId);
 	}
 }
