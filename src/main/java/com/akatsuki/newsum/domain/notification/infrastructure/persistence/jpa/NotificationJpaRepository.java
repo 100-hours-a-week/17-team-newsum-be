3191ee43c49395066b14ab478ad75beef25a1e94
Han Ju Kim <hjkim2902@gmail.com>
2025-06-10 15:33:06 +0900

    refactor: 알림 삭제 DB 로직 추가

--- src/main/java/com/akatsuki/newsum/domain/notification/infrastructure/persistence/jpa/NotificationJpaRepository.java
+++ src/main/java/com/akatsuki/newsum/domain/notification/infrastructure/persistence/jpa/NotificationJpaRepository.java
@@ -1,9 +1,12 @@
 package com.akatsuki.newsum.domain.notification.infrastructure.persistence.jpa;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.akatsuki.newsum.domain.notification.domain.Notification;
 import com.akatsuki.newsum.domain.user.entity.User;
@@ -16,4 +19,8 @@ public interface NotificationJpaRepository extends JpaRepository<Notification, L
 	Optional<Notification> findByIdAndUserId(Long id, Long userId);
 
 	Boolean existsByUserId(Long userId);
+
+	@Modifying(clearAutomatically = true, flushAutomatically = true)
+	@Query("DELETE FROM Notification n WHERE n.createdAt < :threshold")
+	int deleteBefore(@Param("threshold") LocalDateTime threshold);
 }
