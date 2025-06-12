050b66e31b54ccf9c73c7abc7a8ef16868357af2
Han Ju Kim <hjkim2902@gmail.com>
2025-06-12 15:10:40 +0900

    feat: WebClient에서 OpenFeign으로 변경

--- src/main/java/com/akatsuki/newsum/extern/properties/AiServerProperties.java
+++
@@ -1,9 +0,0 @@
-package com.akatsuki.newsum.extern.properties;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "ai-server")
-public record AiServerProperties(
-	String baseUrl
-) {
-}
