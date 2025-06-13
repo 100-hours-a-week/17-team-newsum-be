1c903bcce27fafaca4abea2ebf1778d36f4e9575
HanJu Kim <56250226+rlagkswn00@users.noreply.github.com>
2025-06-13 13:20:30 +0900

    Fix: 웹툰 생성 로직 요청 422 에러 수정 (#211)
    * feat: OpenFeign Log 출력하도록 설정
    
    * feat: 이미지 생성 요청시 요청 body 형식 수정

--- src/main/java/com/akatsuki/newsum/config/OpenFeignConfig.java
+++ src/main/java/com/akatsuki/newsum/config/OpenFeignConfig.java
@@ -1,9 +1,17 @@
 package com.akatsuki.newsum.config;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import feign.Logger;
 
 @Configuration
 @EnableFeignClients(basePackages = "com.akatsuki.newsum")
 public class OpenFeignConfig {
+
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
+	}
 }
--- src/main/java/com/akatsuki/newsum/extern/dto/CreateWebtoonApiRequest.java
+++ src/main/java/com/akatsuki/newsum/extern/dto/CreateWebtoonApiRequest.java
@@ -1,8 +1,10 @@
 package com.akatsuki.newsum.extern.dto;
 
+import java.util.List;
+import java.util.Map;
+
 public record CreateWebtoonApiRequest(
 	String id,
-	String imagePrompts
-
+	List<Map<String, Object>> imagePrompts
 ) {
 }
