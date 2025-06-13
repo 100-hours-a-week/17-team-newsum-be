6437e8a7ffccd1978c2b951d92d9841ed37c1093
HanJu Kim <56250226+rlagkswn00@users.noreply.github.com>
2025-06-13 13:27:16 +0900

    Fix/job wrong request (#212)
    * feat: OpenFeign Log 출력하도록 설정
    
    * feat: 이미지 생성 요청시 요청 body 형식 수정
    
    * fix: 테스트 픽스처 수정

--- src/test/java/com/akatsuki/newsum/fixture/extern/AiServerApiFixture.java
+++ src/test/java/com/akatsuki/newsum/fixture/extern/AiServerApiFixture.java
@@ -1,19 +1,13 @@
 package com.akatsuki.newsum.fixture.extern;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import com.akatsuki.newsum.extern.dto.CreateWebtoonApiRequest;
 
 public class AiServerApiFixture {
 
 	public static CreateWebtoonApiRequest createBasicWebtoonRequest() {
-		return new CreateWebtoonApiRequest("1", createBasicImagePrompts());
-	}
-
-	public static String createBasicImagePrompts() {
-		return """
-			{
-				"scene1": "주인공이 학교에 가는 모습",
-				"scene2": "친구들과 대화하는 장면"
-			}
-			""";
+		return new CreateWebtoonApiRequest("1", new ArrayList<Map<String, Object>>());
 	}
 }
